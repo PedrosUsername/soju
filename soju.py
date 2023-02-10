bdb89cc26bd03b3905cd681bece6fc245bc6abb4
Pedro <pilique11@gmail.com>
2023-02-10 05:20:22 -0300

    isola soju.py de definicoes

--- soju.py
+++ soju.py
@@ -1,17 +1,26 @@
-import os
 import json
+import sys
 
-from utils import utils, ImageMergeStrategy
-from utils.settings import variables
-from moviepy.editor import *
-
+from utils import utils
 
 
 
 
-# TODO - organize code and directories with some fancy design pattern
+
+# TODO - organize code and directories with some design pattern
 # TODO - add video video composition
 # TODO - audio volume controls
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -21,13 +30,13 @@ jsonfilepath = sys.argv[2] if len(sys.argv) > 2 else None
 
 
 
-if(videofilepath is not None and jsonfilepath is None):
-    clip = VideoFileClip(videofilepath, target_resolution=(1080, 1920))
-    print("Soju - clip duration: {}".format(clip.duration))
-    clip.audio.write_audiofile(variables.PATH_TMP_AUDIO, ffmpeg_params=["-ac", "1"])
 
-    list_of_words = utils.voskDescribe(variables.PATH_TMP_AUDIO, variables.PATH_MODEL)
-    os.remove(variables.PATH_TMP_AUDIO)
+
+if(videofilepath is not None and jsonfilepath is None):
+    clip = utils.get_and_prepare_clip_for_vosk_description(videofilepath)
+    print("Soju - clip duration: {}".format(clip.duration))
+
+    list_of_words = utils.voskDescribe()
 
     with open(utils.generate_soju_file_name(videofilepath), 'w') as f:
         f.writelines('{\n\t"data": [' + '\n')
@@ -39,8 +48,12 @@ if(videofilepath is not None and jsonfilepath is None):
     
     clip.close()
 
+
+
+
+
 elif(videofilepath is not None and jsonfilepath is not None):
-    goofy_trigger = "end" if variables.BOOM_AT_WORD_END else "start"
+    goofy_trigger = utils.get_trigger_settings()
 
     describe_json = []
     with open(utils.generate_soju_file_name(videofilepath), 'r') as f:
@@ -48,36 +61,31 @@ elif(videofilepath is not None and jsonfilepath is not None):
 
     describe_goofywords = json.loads(describe_json)["goofywords"]
 
-    clip = VideoFileClip(videofilepath, target_resolution=(1080, 1920))
+    clip = utils.get_and_prepare_clip_for_moviepy_edition(videofilepath)
 
     for word in describe_goofywords:
-        goofy_image = '{0}{1}'.format(variables.DEFAULT_IMAGE_PATH, word["image"]) if word["image"] is not None else variables.DEFAULT_NULL_IMAGE_FILE
-        goofy_audios = word["audio"] if word["audio"] is not None else []
+        image = utils.get_goofy_image(word)
 
-        image = ImageClip(goofy_image, duration= variables.MAX_IMAGE_DURATION)
-        image = image.subclip(0, image.end).set_pos(("center","center")).resize((1920, 1080)).crossfadeout(.5)
-        
         uppper_half = clip.subclip(word[goofy_trigger], clip.end)
         bottom_half = clip.subclip(clip.start, word[goofy_trigger])
 
-        if word["imageconcatstrategy"] == ImageMergeStrategy.CONCAT_ENUM:
-            uppper_half = concatenate_videoclips([image, uppper_half])
-            for word_again in describe_goofywords:
-                word_again["start"] = word_again["start"] + variables.MAX_IMAGE_DURATION
-                word_again["end"] = word_again["end"] + variables.MAX_IMAGE_DURATION
-
-        else:
-            uppper_half = CompositeVideoClip([uppper_half, image])
+        uppper_half = utils.merge_visuals(
+            uppper_half,
+            image,
+            word,
+            describe_goofywords
+        )
         
+        goofy_audios = word["audio"] if word["audio"] is not None else []
         for goofy_audio in goofy_audios:
-            goofy_audio = '{0}{1}'.format(variables.DEFAULT_AUDIO_PATH, goofy_audio) if goofy_audio is not None else variables.DEFAULT_NULL_AUDIO_FILE
-            audio = AudioFileClip(goofy_audio)
-            audio = audio.subclip(0, variables.MAX_AUDIO_DURATION) if audio.duration > variables.MAX_AUDIO_DURATION else audio.subclip(0, audio.end)
-            audio = audio.fx(afx.audio_fadeout, audio.duration * (2/3))
+            audio = utils.get_goofy_audio(goofy_audio)
 
-            uppper_half.audio = CompositeAudioClip([uppper_half.audio, audio])
+            uppper_half.audio = utils.merge_sounds(
+                uppper_half,
+                audio,
+            )
         
-        clip = concatenate_videoclips([bottom_half, uppper_half])
+        clip = utils.final_merge(bottom_half, uppper_half)
 
     print("Soju - final clip duration: {0}".format(clip.duration))
     clip.write_videofile(
--- utils/utils.py
+++ utils/utils.py
@@ -5,11 +5,18 @@ import wave
 import os
 
 from vosk import Model, KaldiRecognizer
+from moviepy.editor import *
 from itertools import cycle
 
+from .settings import variables
 from . import Word as custom_word
 from . import ImageMergeStrategy
-from .settings import variables
+
+
+
+
+
+
 
 
 
@@ -21,6 +28,7 @@ from .settings import variables
 
 def file_is_a_good_choice(image_file):
     return os.path.isfile('{0}{1}'.format(variables.DEFAULT_IMAGE_PATH, image_file)) and image_file not in variables.IGNORE_IMAGE_FILE_LIST
+
 
 
 
@@ -39,10 +47,14 @@ def getRandomizedImageFileName(image_files):
 
 
 
+
+
 def getRandomizedAudioFileNames():
     audio_files = os.listdir(variables.DEFAULT_AUDIO_PATH)
     random.shuffle(audio_files)
     return [aud for aud in audio_files if os.path.isfile('{0}{1}'.format(variables.DEFAULT_AUDIO_PATH, aud)) and aud not in variables.IGNORE_AUDIO_FILE_LIST and aud not in variables.DEFAULT_AUDIO_FILE]
+
+
 
 
 
@@ -57,6 +69,7 @@ def constructWord(obj, image_files):
 
 
 
+
 def get_base_file_name_from(videofilepath):
     filename = ntpath.basename(videofilepath)
     head, tail = filename[::-1].split(".", 1)
@@ -65,9 +78,11 @@ def get_base_file_name_from(videofilepath):
 
 
 
+
 def generate_soju_file_name(videofilepath):
     videofilename = get_base_file_name_from(videofilepath)
     return "{}{}.soju.json".format(variables.PATH_DEFAULT_JSON_FILE, videofilename)
+
 
 
 
@@ -80,12 +95,87 @@ def generate_output_file_name(videofilepath):
 
 
 
+def get_goofy_image(word):
+    goofy_image = '{0}{1}'.format(variables.DEFAULT_IMAGE_PATH, word["image"]) if word["image"] is not None else variables.DEFAULT_NULL_IMAGE_FILE
+    image = ImageClip(goofy_image, duration= variables.MAX_IMAGE_DURATION)
+    return image.subclip(0, image.end).set_pos(("center","center")).resize((1920, 1080)).crossfadeout(.5)
 
 
-def voskDescribe(fil, mod):
+
+
+
+def get_goofy_audio(element):
+    goofy_audio = '{0}{1}'.format(variables.DEFAULT_AUDIO_PATH, element) if element is not None else variables.DEFAULT_NULL_AUDIO_FILE
+    audio = AudioFileClip(goofy_audio)
+    audio = audio.subclip(0, variables.MAX_AUDIO_DURATION) if audio.duration > variables.MAX_AUDIO_DURATION else audio.subclip(0, audio.end)
+    return audio.fx(afx.audio_fadeout, audio.duration * (2/3))
+
+
+
+
+
+def clip_extend(goofy_words):
+    for word in goofy_words:
+        word["start"] = word["start"] + variables.MAX_IMAGE_DURATION
+        word["end"] = word["end"] + variables.MAX_IMAGE_DURATION
+
+
+
+
+
+def merge_visuals(upper_half, image, word, goofy_words):
+    if word["imageconcatstrategy"] == ImageMergeStrategy.CONCAT_ENUM:
+        result = concatenate_videoclips([image, upper_half])
+        clip_extend(goofy_words)
+    else:
+        result = CompositeVideoClip([upper_half, image])
+    return result
+
+
+
+
+
+def merge_sounds(upper_half, audio):
+    return CompositeAudioClip([upper_half.audio, audio])
+
+
+
+
+
+def get_and_prepare_clip_for_vosk_description(videofilepath):
+    result = VideoFileClip(videofilepath, target_resolution=(1080, 1920))
+    result.audio.write_audiofile(variables.PATH_TMP_AUDIO, ffmpeg_params=["-ac", "1"])
+    return result
+
+
+
+
+
+def get_and_prepare_clip_for_moviepy_edition(videofilepath):
+    return VideoFileClip(videofilepath, target_resolution=(1080, 1920))
+
+
+
+
+
+def final_merge(bottom_half, uppper_half):
+    return concatenate_videoclips([bottom_half, uppper_half])
+
+
+
+
+
+def get_trigger_settings():
+    return "end" if variables.BOOM_AT_WORD_END else "start"
+
+
+
+
+
+def voskDescribe():
     image_files = os.listdir(variables.DEFAULT_IMAGE_PATH)
-    model = Model(mod)
-    wf = wave.open(fil, "rb")
+    model = Model(variables.PATH_MODEL)
+    wf = wave.open(variables.PATH_TMP_AUDIO, "rb")
     rec = KaldiRecognizer(model, wf.getframerate())
     rec.SetWords(True)
 
@@ -116,6 +206,6 @@ def voskDescribe(fil, mod):
                 image_files.remove(new_word.image)
             word_list.append(new_word)  # and add it to list
     wf.close()  # close audiofile
-
+    os.remove(variables.PATH_TMP_AUDIO)
     return word_list
 
