766179eacad114c78327e43aa1de1161d0801b08
Pedro <pilique11@gmail.com>
2023-04-15 01:43:33 -0300

    update variables.py

--- utils/settings/variables.py
+++ utils/settings/variables.py
@@ -2,42 +2,75 @@
 # end     -> boom will be inserted at ["boomers"]["word"]["end"] seconds
 DEFAULT_BOOM_TRIGGER = "start"
 
-# COMPOSE   -> image/video will overlay original clip
-# CONCAT    -> image/video will interrupt video to show up
-# F_COMPOSE -> image (doesn't work with videos) will overlay original clip (faster than COMPOSE but it has no animations, nor support for positioning)
+
+
+
+
+
+
+
+
+
+# ########################################## soju.json IMAGE CONF.
+
+# COMPOSE
+#       -> image/video will overlay original clip
+# CONCAT
+#       -> image/video will interrupt video to show up
+# F_COMPOSE
+#       -> image (doesn't work with videos) will overlay original clip.
+#       it's faster than COMPOSE but it has lots of limitations
+
 DEFAULT_IMAGE_CONCAT_STRATEGY = "F_COMPOSE"      
 
-# image confs
-DEFAULT_IMAGE_PATH = "./assets/image/mommy/"
-DEFAULT_IMAGE_FILE = "vibecheckemojialt.png"
-DEFAULT_NULL_IMAGE_FILE = "./assets/image/null.png"
-ALLOW_IMAGE_REPETITION = False                  # ignored when not CHOOSE_IMAGE_AT_RANDOM 
-CHOOSE_IMAGE_AT_RANDOM = 1                     # 0     -> use image DEFAULT_IMAGE_FILE for every word
-                                               # n > 0 -> use Random images for every word
+DEFAULT_IMAGE_FOLDER = "./assets/image/mommy/"
+ALLOW_IMAGE_REPETITION = False
+IGNORE_IMAGE_FILE_LIST = []
+
+DEFAULT_IMAGE_FILE = None
+
 MAX_IMAGE_DURATION = 0.36
 DEFAULT_IMAGE_VOLUME = 0
+
+DEFAULT_NULL_IMAGE_FILE = "./assets/image/null.png"
+
 DEFAULT_IMAGE_RESOLUTION_HEIGHT = 1080               # 1080 px
 DEFAULT_IMAGE_RESOLUTION_WIDTH = 1920                # 1920 px
 DEFAULT_IMAGE_POSITION_X = 0                         # float relative to screen size
 DEFAULT_IMAGE_POSITION_Y = 0                         # https://zulko.github.io/moviepy/getting_started/compositing.html?highlight=position#positioning-clips
 
-IGNORE_IMAGE_FILE_LIST = [
-    DEFAULT_NULL_IMAGE_FILE
-]
+# ################################################################
 
 
-# audio confs
-DEFAULT_AUDIO_PATH = "./assets/audio/"
-DEFAULT_AUDIO_FILE = ["vineboom.mp3"]
-DEFAULT_NULL_AUDIO_FILE = "./assets/audio/null.mp3"
-CHOOSE_AUDIO_AT_RANDOM = 0                     # 0     -> add only DEFAULT_AUDIO_FILE sounds
-                                               # n > 0 -> add DEFAULT_AUDIO_FILE + n random sounds
+
+
+
+
+
+
+
+
+# ########################################## soju.json AUDIO CONF.
+
+DEFAULT_AUDIO_FOLDER = "./assets/audio/cringe/"
+ALLOW_AUDIO_REPETITION = True
+IGNORE_AUDIO_FILE_LIST = []
+
+DEFAULT_AUDIO_FILE = "vineboom.mp3"
+
 MAX_AUDIO_DURATION = 0.9
 DEFAULT_SOUND_VOLUME = 1
 
-IGNORE_AUDIO_FILE_LIST = [
-    DEFAULT_NULL_AUDIO_FILE
-]
+DEFAULT_NULL_AUDIO_FILE = "./assets/audio/null.mp3"
+
+# ################################################################
+
+
+
+
+
+
+
 
 
 # general confs
@@ -49,6 +82,10 @@ PATH_MODEL = "./models/en-model"
 DEFAULT_OUTPUT_PATH = "./"
 
 PATH_DEFAULT_JSON_FILE = "./assets/json/"
+
+
+
+
 
 
 
--- utils/vosk_utils.py
+++ utils/vosk_utils.py
@@ -4,7 +4,6 @@ import json
 import os
 
 from vosk import Model, KaldiRecognizer
-from itertools import cycle
 
 from . import Boomer as custom_b
 from .settings import variables
@@ -13,34 +12,28 @@ from .settings import variables
 
 
 
-def file_is_a_good_choice(image_file):
-    return os.path.isfile('{0}{1}'.format(variables.DEFAULT_IMAGE_PATH, image_file)) and image_file not in variables.IGNORE_IMAGE_FILE_LIST
+def image_file_is_a_good_choice(image_file= ""):
+    return os.path.isfile('{0}{1}'.format(variables.DEFAULT_IMAGE_FOLDER, image_file)) and image_file not in variables.IGNORE_IMAGE_FILE_LIST
+
+def audio_file_is_a_good_choice(audio_file= ""):
+    return os.path.isfile('{0}{1}'.format(variables.DEFAULT_AUDIO_FOLDER, audio_file)) and audio_file not in variables.IGNORE_AUDIO_FILE_LIST
+
+def getFile(files= [], default_file= None):
+    if default_file != None:
+        return default_file
+    
+    elif len(files) > 0:
+        return random.choice(files)
+    
+    else:
+        return None
 
 
 
-def getRandomizedImageFileName(image_files):
-    flag = False
 
-    while flag is False:
-        try:
-            image_file = random.choice(image_files)
-            flag = True if file_is_a_good_choice(image_file) else False
-        except:
-            image_file = None
-            flag = True
-    return image_file
-
-
-def getRandomizedAudioFileNames():
-    audio_files = os.listdir(variables.DEFAULT_AUDIO_PATH)
-    random.shuffle(audio_files)
-    return [aud for aud in audio_files if os.path.isfile('{0}{1}'.format(variables.DEFAULT_AUDIO_PATH, aud)) and aud not in variables.IGNORE_AUDIO_FILE_LIST and aud not in variables.DEFAULT_AUDIO_FILE]
-
-
-
-def constructBoomer(obj, image_files):
-    image_name = getRandomizedImageFileName(image_files) if variables.CHOOSE_IMAGE_AT_RANDOM == True else variables.DEFAULT_IMAGE_FILE
-    audio_names = cycle(getRandomizedAudioFileNames())
+def buildBoomer(obj, image_files, audio_files):
+    image_file = getFile(image_files, variables.DEFAULT_IMAGE_FILE)
+    audio_file = getFile(audio_files, variables.DEFAULT_AUDIO_FILE)
 
     obj["word"] = {
         "content": obj["word"],
@@ -49,7 +42,7 @@ def constructBoomer(obj, image_files):
     }
 
     obj["image"] = {
-        "file": image_name,
+        "file": image_file,
         "conf": {
             "boom_trigger": variables.DEFAULT_BOOM_TRIGGER,
             "height": variables.DEFAULT_IMAGE_RESOLUTION_HEIGHT,
@@ -65,7 +58,7 @@ def constructBoomer(obj, image_files):
     }
 
     obj["audio"] = {
-        "files": variables.DEFAULT_AUDIO_FILE + [next(audio_names) for i in range(variables.CHOOSE_AUDIO_AT_RANDOM)],
+        "file": audio_file,
         "conf": {
             "max_duration": variables.MAX_AUDIO_DURATION,
             "volume": variables.DEFAULT_SOUND_VOLUME
@@ -77,7 +70,11 @@ def constructBoomer(obj, image_files):
 
 
 def voskDescribe(audio_file_path= ""):
-    image_files = os.listdir(variables.DEFAULT_IMAGE_PATH)
+    image_files = os.listdir(variables.DEFAULT_IMAGE_FOLDER)
+    valid_image_files = [file for file in image_files if image_file_is_a_good_choice(file)]
+    audio_files = os.listdir(variables.DEFAULT_AUDIO_FOLDER)
+    valid_audio_files = [file for file in audio_files if audio_file_is_a_good_choice(file)]
+
     model = Model(variables.PATH_MODEL)
     wf = wave.open(audio_file_path, "rb")
     rec = KaldiRecognizer(model, wf.getframerate())
@@ -104,9 +101,14 @@ def voskDescribe(audio_file_path= ""):
             # {'text': ''}
             continue
         for obj in sentence['result']:
-            new_word = constructBoomer(obj, image_files)
-            if new_word.image is not None and new_word.image["file"] is not None and variables.ALLOW_IMAGE_REPETITION is not True and variables.CHOOSE_IMAGE_AT_RANDOM > 0:
-                image_files.remove(new_word.image["file"])
+            new_word = buildBoomer(obj, valid_image_files, valid_audio_files)
+
+            if variables.ALLOW_IMAGE_REPETITION is False and new_word.image["file"] in valid_image_files:
+                valid_image_files.remove(new_word.image["file"])
+
+            if variables.ALLOW_AUDIO_REPETITION is False and new_word.audio["file"] in valid_audio_files:
+                valid_audio_files.remove(new_word.audio["file"])
+
             word_list.append(new_word)  # and add it to list
     wf.close()  # close audiofile
     return word_list
